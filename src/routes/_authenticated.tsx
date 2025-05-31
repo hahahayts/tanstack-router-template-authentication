57f99dfaa55c4d72f798febad819d5706b249a14
mrf3v3r <jojehterce@gmail.com>
2025-05-31 15:23:54 +0800

    feat: add logout functionality to authenticated layout with button integration

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -1,5 +1,13 @@
 // src/routes/_authenticated.tsx
-import { createFileRoute, redirect, Outlet } from "@tanstack/react-router";
+import { logoutUser } from "@/actions/auth";
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/context/AuthContext";
+import {
+  createFileRoute,
+  redirect,
+  Outlet,
+  useNavigate,
+} from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated")({
   component: AuthenticatedLayout,
@@ -15,8 +23,19 @@ export const Route = createFileRoute("/_authenticated")({
 });
 
 function AuthenticatedLayout() {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+  const handleLogout = async () => {
+    await logoutUser();
+    logout();
+    navigate({
+      to: "/auth/login",
+      replace: true,
+    });
+  };
   return (
     <div>
+      <Button onClick={handleLogout}>Logout</Button>
       <Outlet />
     </div>
   );
